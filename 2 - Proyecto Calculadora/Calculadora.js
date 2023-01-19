3f2ddd2f73065dc7ac62f9e9caca0767a0558e61
fedemoretto11 <fedemoretto94@gmail.com>
2023-01-19 14:47:44 -0300

    Calculadora y ToDo terminadas

---
+++ 2 - Proyecto Calculadora/Calculadora.js
@@ -0,0 +1,24 @@
+class Calculadora {
+
+    sumar(num1, num2) {
+        return num1 + num2
+    }
+
+    restar(num1, num2) {
+        return num1 - num2
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2
+    }
+
+    // potencia(num1, num2) {
+    //     return num1 ** num2
+    // }
+
+
+}
--- 3 - To Do List/script.js
+++ 3 - To Do List/script.js
@@ -1,12 +1,53 @@
-const tareas = document.querySelector(".toDo");
-const porHacer = document.querySelector(".tareas");
-const icono = document.querySelector(".icono");
-const ul = document.querySelector("ul");
-const li = document.createElement("li");
+const dateNumber = document.getElementById('dateNumber');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
+const dateText = document.getElementById('dateText');
+
+const taskContainer = document.getElementById('taskContainer');
+
+const setDate = () => {
+    const date = new Date();
+
+    dateNumber.textContent = date.toLocaleString('es', { day: 'numeric' });
+    dateMonth.textContent = date.toLocaleString('es', { month: 'short' });
+    dateYear.textContent = date.toLocaleString('es', { year: 'numeric' });
+    dateText.textContent = date.toLocaleString('es', { weekday: 'long' });   
+    
+
+};
 
 
-icono.addEventListener("click", (e) => {
-    li.appendChild(document.createTextNode(tareas.value));
-    ul.appendChild(li);
-    tareas = " ";
-})
+const addNewTask = event => {
+    event.preventDefault();
+    const { value } = event.target.taskText;
+    if(!value) return;
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value;
+    taskContainer.prepend(task);
+    event.target.reset();
+};
+
+const changeTaskState = event => {
+    event.target.classList.toggle('done')
+};
+
+const order = () => {
+    const done = [];
+    const toDo = [];
+    taskContainer.childNodes.forEach(el => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el);
+    })
+    return [...toDo, ...done]
+}
+
+const renderOrderedTasks = () => {
+    order().forEach(el => { taskContainer.appendChild(el)})
+}
+
+
+
+
+
+setDate();
